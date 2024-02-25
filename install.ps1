fdfb9d8ed73ca3e03bcb5546a814fe472536a748
D Jeffrey <44452180+D-Jeffrey@users.noreply.github.com>
2024-02-24 22:33:22 -0700

    Test driving auto install and auto setup 2

--- install.ps1
+++ install.ps1
@@ -1,7 +1,7 @@
 $scriptDir = join-path -Path ${env:ProgramData} -childpath 'EmbroideryOrganize' 
 function Get-FileTo($file) {
     $url = "https://raw.githubusercontent.com/D-Jeffrey/Embroidery-File-Organize/main/$file"
-    $downloadFromGitHub = Invoke-WebRequest -Uri $url
+    $downloadFromGitHub = Invoke-WebRequest -Uri "$url"
     $downloadFile = Join-Path -Path $scriptDir -ChildPath $file
     Set-Content -Path $downloadFile -Value $downloadFromGitHub.Content
 }
@@ -12,6 +12,6 @@ $pushscript = 'EmbroideryCollection-Cleanup.ps1'
 $pushscript = 'testcall.ps1'
 Get-FileTo -file $pushscript
 $scriptname = Join-Path -Path $scriptDir -ChildPath $pushscript
-Get-FileTo -file 'embroiderymanager.ico'
+Get-FileTo -file 'EmbroideryManager.ico'
 
 Powershell -NoLogo -ExecutionPolicy Bypass -File $scriptname -setup
